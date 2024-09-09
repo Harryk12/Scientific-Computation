169ba76572ca1b6a33b643f175ae8160a9e60a4a
hkangt <hkangt@isp03.tacc.utexas.edu>
2024-09-08 21:05:48 -0500

    adding comments

--- hw3/looping.cpp
+++ hw3/looping.cpp
@@ -8,19 +8,19 @@ using std::endl;
 
 int main() {
   int longestsequence=0;
-  for(int i=1;i<=1000;i++){
+  for(int i=1;i<=1000;i++){   // looping through all starting numbers
     int currentnum =i;
-    int startingnum=currentnum;
-    int sequencecount=1;
-    while(currentnum>1){
+    int startingnum=currentnum;   //save for later
+    int sequencecount=1;   //initialize length of current sequence
+    while(currentnum>1){    //actual algorithm
       if(currentnum%2==0){
 	currentnum=currentnum/2;
       } else{
 	currentnum=3*currentnum+1;
       }
-      sequencecount++;
+      sequencecount++;    //update the count of the sequence
     }
-    if (sequencecount>longestsequence){
+    if (sequencecount>longestsequence){ //conditional to update the new longest sequence with the current one if bigger
       longestsequence=sequencecount;
       cout<<"The new longest sequence of "<<longestsequence<<" started with "<<startingnum<<endl;
     }
