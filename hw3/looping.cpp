3c6071bc6f7f45fe84ad3a2c63eca57eb5286a65
hkangt <hkangt@isp03.tacc.utexas.edu>
2024-09-08 21:01:09 -0500

    hw3 push

--- hw3/looping.cpp
+++ hw3/looping.cpp
@@ -1,20 +1,16 @@
+//Harry Singh Kang eid:hsk697  tacc:hkangt
 #include <iostream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-int read_number(){
-  int n;
-  cin>>n;
-  return n;
-}
 
 int main() {
-  int currentnum=read_number();
-  int startingnum=currentnum;
   int longestsequence=0;
   for(int i=1;i<=1000;i++){
+    int currentnum =i;
+    int startingnum=currentnum;
     int sequencecount=1;
     while(currentnum>1){
       if(currentnum%2==0){
@@ -26,7 +22,7 @@ int main() {
     }
     if (sequencecount>longestsequence){
       longestsequence=sequencecount;
-      cout<<"Starting number for this new longest sequence of "<<longestsequence<<" is "<<startingnum<<endl;
+      cout<<"The new longest sequence of "<<longestsequence<<" started with "<<startingnum<<endl;
     }
   }
   return 0;
